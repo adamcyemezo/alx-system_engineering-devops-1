bf7bebc7b7008797e5450bdc937f4252c4469106
sharifhabiman <hdidiersharif@gmail.com>
2022-04-01 10:27:08 +0200

    puppet code

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,8 @@
-# Configures a server to act as a load balancer for 2 web servers
-# using HAProxy
-
-exec {'configure_haproxy':
-    command  => './0-custom_http_response_header',
-    provider => 'shell'
+# Automation: creates a custom HTTP header response with Puppet.
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
-
