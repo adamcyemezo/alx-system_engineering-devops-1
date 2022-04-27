6b81250f7dc8e513fc5ae778b9874d1cb508ca97
sharifhabiman <hdidiersharif@gmail.com>
2022-04-27 12:26:53 +0200

    correction

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,10 +6,12 @@ import requests
 def top_ten(subreddit):
     """print title of the 10 hottest posts on  a given subredit"""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    {
+
+    headers = {
         "User-Agent": "0x16-api_advanced:project:\
 v1.0.0 (by /u/firdaus_cartoon_jr)"
     }
+    
     params = {
         "limit": 10
     }
